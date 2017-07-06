a63fc45389b1baedd813647b6c4801d022bb5e8e
Christoforos <chrischaralambous14@gmail.com>
2017-07-06 13:57:32 +0300

    Semver versioning

--- src/main/java/elchris414/dalon/DalonStrings.java
+++ src/main/java/elchris414/dalon/DalonStrings.java
@@ -3,5 +3,5 @@ package elchris414.dalon;
 public class DalonStrings {
 	public static final String MODID = "dalon";
 	public static final String MODNAME = "Dalon";
-	public static final String VERSION = "0a";
+	public static final String VERSION = "0.1.0";
 }
